a403cb34fb9717edf5167d95237127f76b3febe1
Nainjain26 <naina.jain2606@gmail.com>
2024-11-23 00:00:35 +0530

    h

--- src/app/components/Option.tsx
+++ src/app/components/Option.tsx
@@ -4,14 +4,20 @@ import Link from "next/link";
 const Option = () => {
   return (
     <div className="mt-10  bg-blue-50">
-        <h1 className="text-center text-3xl md:text-4xl font-semibold underline md:pt-10 decoration-blue-500 decoration-4 underline-offset-6 leading-tight">Our Services</h1>
-        <p className="max-w-7xl mx-auto text-center pt-5 text-gray-700 md:text-xl mb-20">At Sahaj Research Services , we specialize in comprehensive financial services, including stocks, futures, options, cash, and commodities. Our expert team is dedicated to guiding you through the complexities of the financial markets, ensuring tailored solutions to meet your investment goals. Trust us to provide the insights and support you need for a secure and prosperous financial future.</p>
+      <h1 className="text-center text-3xl md:text-4xl font-semibold underline md:pt-10 decoration-blue-500 decoration-4 underline-offset-6 leading-tight">
+        Our Services
+      </h1>
+      <p className="max-w-7xl mx-auto text-center pt-5 text-gray-700 md:text-xl mb-20">
+        Sahaj Research Services offers expert financial solutions in stocks,
+        futures, options, and commodities. Our dedicated team ensures
+        personalized guidance, simplifying market complexities for secure,
+        goal-oriented investments and lasting prosperity.
+      </p>
       <div className="md:flex justify-evenly m-10 gap-10">
         <div className=" p-10 shadow-xl cursor-pointer rounded-2xl transition-all hover:scale-110 hover:bg-white mb-8 text-center bg">
           <img
             src="/Promoting Generosity Through Charitable Giving Initiatives 3D Scene.png"
             alt=""
-            
             className="mx-auto h-[120px] md:h-[250px] transform transition-transform duration-500 hover:scale-110 hover:rotate-6"
           />
           <h1 className="text-2xl md:text-4xl text-blue-500 font-semibold py-5">
@@ -38,8 +44,7 @@ const Option = () => {
             stock market, or of a subset of a stock market.
           </p>
         </div>
-        
-        
+
         <div className=" p-10 shadow-xl cursor-pointer rounded-2xl transition-all hover:scale-110 hover:bg-white mb-8 text-center bg ">
           <img
             src="/Ensuring Security For Cryptocurrencies Holdings 3D Scene.png"
@@ -61,7 +66,6 @@ const Option = () => {
           <img
             src="/Utilizing Cryptocurrencies In Cloud Computing 3D Scene.png"
             alt=""
-            
             className="mx-auto h-[120px] md:h-[250px] transform transition-transform duration-500 hover:scale-110 hover:rotate-6"
           />
           <h1 className="text-2xl md:text-4xl text-blue-500 font-semibold py-5">
@@ -73,10 +77,18 @@ const Option = () => {
           </p>
         </div>
       </div>
-     <p className="text-center text-xl pb-10 md:text-2xl transition-all hover:scale-105 duration-150 md:mb-11 "> <Link href={"/pricing"} className="mx-auto border p-3 rounded-2xl bg-blue-500 text-white  hover:bg-blue-600 "> Know More  </Link></p>
+      <p className="text-center text-xl pb-10 md:text-2xl transition-all hover:scale-105 duration-150 md:mb-11 ">
+        {" "}
+        <Link
+          href={"/pricing"}
+          className="mx-auto border p-3 rounded-2xl bg-blue-500 text-white  hover:bg-blue-600 "
+        >
+          {" "}
+          Know More{" "}
+        </Link>
+      </p>
     </div>
   );
 };
 
 export default Option;
- 
--- src/app/components/Work.tsx
+++ src/app/components/Work.tsx
@@ -10,67 +10,64 @@ const Work = () => {
         How does it work...?
       </h1>
       <p className="text-center text-gray-700 mb-10  md:text-xl max-w-5xl mx-auto">
-      Connect with the perfect analyst, select a convenient date and time, and enjoy personalized guidance from the comfort of your home, ensuring a seamless and stress-free experience tailored to your needs.
+        Connect with the perfect analyst, select a convenient date and time, and
+        enjoy personalized guidance from the comfort of your home, ensuring a
+        seamless and stress-free experience tailored to your needs.
       </p>
-      <div className="md:grid grid-cols-2  md:mt-20  ">
-      <div className="items-center  mx-10">
+      <div className="md:grid grid-cols-2  md:mt-16  ">
+        <div className="items-center  mx-10">
           <img
             src="/job-5382501.jpg"
             alt=""
-            className=" h-[280px] md:h-[450px] rounded-2xl"
+            className=" h-[280px] md:h-[480px] rounded-2xl object-cover"
           />
         </div>
         <div className="mx-10 flex flex-col items-center justify-center">
           <div className="flex items-center  md:border p-5 bg md:mt-5 md:rounded-xl md:transition md:hover:scale-105 md:hover:shadow-xl md:hover:bg-slate-50 cursor-pointer">
             <p className="bg-white p-5 shadow-xl text-center text-white text-2xl md:text-4xl font-semibold mx-auto rounded-full">
               {" "}
-              {<MdOutlineFilter1 className="text-blue-500"/>}
+              {<MdOutlineFilter1 className="text-blue-500" />}
             </p>
             <div className="ml-3">
               <p className="text-xl md:text-2xl pb-2  font-semibold ">
                 Get matched with the right Analyst for you.
               </p>
               <p className="text-gray-700">
-                From immunizations to checkups and preventive care, exams, our
-                primary care physicians and providers work to keep you and your
-                whole family healthy and strong each and every day.
+                Find your perfect stock market analyst. Receive expert guidance,
+                personalized strategies, and insights to achieve your investment
+                goals confidently.
               </p>
             </div>
           </div>
           <div className="flex items-center  md:border p-5 bg md:mt-5 md:rounded-xl md:transition md:hover:scale-105 md:scale-105 md:shadow-xl md:bg-slate-50 cursor-pointer">
             <p className="bg-white p-5 shadow-xl text-center text-white text-2xl md:text-4xl font-semibold mx-auto rounded-full ">
               {" "}
-              {<MdOutlineFilter2  className="text-blue-500"/>}
+              {<MdOutlineFilter2 className="text-blue-500" />}
             </p>
             <div className="ml-3">
               <p className="text-xl md:text-2xl pb-2  font-semibold">
                 Choose a date and time.
               </p>
               <p className="text-gray-700">
-                From immunizations to checkups and preventive care, exams, our
-                primary care physicians and providers work to keep you and your
-                whole family healthy and strong each and every day.
+              Select a convenient date and time for your consultation. Schedule seamlessly to connect with an expert analyst at your preferred availability.
               </p>
             </div>
           </div>
           <div className="flex items-center  md:border p-5 bg md:mt-5 md:rounded-xl md:transition md:hover:scale-105 md:hover:shadow-xl md:hover:bg-slate-50 cursor-pointer">
             <p className="bg-white p-5 shadow-xl text-center text-white text-2xl md:text-4xl font-semibold mx-auto rounded-full">
               {" "}
-              {<MdOutlineFilter3 className="text-blue-500"/>}
+              {<MdOutlineFilter3 className="text-blue-500" />}
             </p>
             <div className="ml-3">
               <p className="text-xl md:text-2xl pb-2  font-semibold">
                 Meet in the comfort of your own home.
               </p>
               <p className="text-gray-700">
-                From immunizations to checkups and preventive care, exams, our
-                primary care physicians and providers work to keep you and your
-                whole family healthy and strong each and every day.
+              Enjoy expert consultations from the comfort of your home. Connect online with experienced analysts for personalized guidance and market insights.
               </p>
             </div>
           </div>
         </div>
-        
       </div>
     </div>
   );
